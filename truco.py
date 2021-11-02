c853d4a57bb223e52bd3ecd49bcad399687883d2
santiagoemens <santiagoemenss@gmail.com>
2021-11-02 20:54:01 -0300

    arreglado

--- truco.py
+++ truco.py
@@ -1,13 +1,15 @@
 from carta import cartas 
 from ordenador import ordenador 
-from random import randint
+import random
 from abc import ABCMeta
 
 
 class truco (metaclass=ABCMeta):
     def jugar():
-        ucartas=[randint(1,14),randint(1,14),randint(1,14)]
-        ocartas=[randint(1,14),randint(1,14),randint(1,14)]
+        listacartas=(1,1,1,1,2,2,2,2,3,3,3,3,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,9,9,9,9,10,10,10,10,11,12,13,14)
+        maso=random.sample(listacartas,6)
+        ucartas=[maso[0],maso[1],maso[2]]
+        ocartas=[maso[3],maso[4],maso[5]]
         contadoruser=0
         contadorordenador=0
         ganador=0 #1 si es user,2 si es ordenador
@@ -21,7 +23,8 @@ class truco (metaclass=ABCMeta):
                 ganador=2
             else:             
                 truco.mostrarmano(ucartas)
-                cartajugada=truco.jugarCarta(ucartas)                              
+                pos=truco.jugarCarta(ucartas)
+                cartajugada=truco.cartaajugar(pos,ucartas)                            
                 ucartas=truco.eliminarcarta(ucartas,cartajugada)
                 cartaordenador=ordenador.ordenador_jugar(ocartas,cartajugada) 
                 ocartas=truco.eliminarcarta(ocartas,cartaordenador)      
@@ -44,16 +47,29 @@ class truco (metaclass=ABCMeta):
 
     #Muestra las cartas disponibles
     def mostrarmano(ucartas):
+        numero=1
         for carta in ucartas:
-            cartas.ajugar(carta)
+            print(str(numero)+": "+cartas.mostrarcarta(carta))
+            numero=numero+1
 
     #Controla que el jugador juegue una carta que tenga    
-    def jugarCarta(ucartas): 
+    def jugarCarta(ucartas):
+        bool=False 
         cartajugada = int(input("carta a jugar: ")) 
-        while cartajugada not in ucartas:
-            if cartajugada not in ucartas:
-                cartajugada = int(input("Elige una carta que tengas :"))
-        return cartajugada              
+        while bool==False:
+            if cartajugada==1:
+                bool=True   
+                return cartajugada
+            if cartajugada==2:
+                bool=True   
+                return cartajugada  
+            if cartajugada==3:
+                bool=True   
+                return cartajugada
+            else:
+                cartajugada = int(input("Ingrese 1,2 o 3: "))
+
+                        
             
     #Elimina la carta jugada    
     def eliminarcarta(ucartas,cartajugada):
@@ -66,3 +82,11 @@ class truco (metaclass=ABCMeta):
         if cartajugada==ucartas[2]: 
             ucartas.pop(2)              
             return ucartas
+    
+    def cartaajugar(cartajugada,ucartas): 
+        if cartajugada==1: 
+            return ucartas[0]
+        if cartajugada==2:                         
+            return ucartas[1]
+        if cartajugada==3:                           
+            return ucartas[2]
