778e64dd7a3b605385b8fe2d1217adf2805c125f
santiagoemens <santiagoemenss@gmail.com>
2021-11-01 23:09:03 -0300

    1

--- ordenador.py
+++ ordenador.py
@@ -5,8 +5,10 @@ class ordenador (metaclass=ABCMeta):
     def ordenador_jugar(ocartas,cartajugada):  
         i=0
         while i<=len(ocartas):
-            if  ocartas[i]>=cartajugada:
+            if  ocartas[i]>cartajugada:
                 cartaordenador=ocartas[i]                        
+            elif ocartas[i]==cartajugada:
+                cartaordenador=ocartas[i]
             else:
                 cartaordenador=ocartas[0]
-            return cartaordenador  
+        return cartaordenador  
