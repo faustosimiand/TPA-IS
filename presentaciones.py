448c3abc24a0d94a6668e5acb6018df5007a7c48
santiagoemens <santiagoemenss@gmail.com>
2021-11-01 22:39:03 -0300

    1

--- presentaciones.py
+++ presentaciones.py
@@ -4,20 +4,23 @@ from abc import ABCMeta
 class ui (metaclass=ABCMeta):
 
     def inicio(): #muestra la primer pantalla
-        print("--------------------------------------")
-        print("-                                    -")
-        print("-                                    -")
-        print("-                                    -")
-        print("-          JUEGO DE TRUCO            -")        
-        print("-                                    -")
-        print("-                                    -")
-        print("-                                    -")
-        print("--------------------------------------")
-        input("       PULSA ENTER PARA INGRESAR      ")
+        print("---------------------------------------")
+        print("-                                     -")       
+        print("-           JUEGO DE TRUCO            -")        
+        print("-                                     -")         
+        input("-------PULSA ENTER PARA INGRESAR-------")
 
     
     def mostrarganador(ganador):
         if ganador==1:
-            print("Has ganado")
+            print("---------------------------------------")
+            print("-                                     -")        
+            print("-             HAS GANADO              -")        
+            print("-                                     -")                     
+            print("---------------------------------------")
         elif ganador==2:
-            print("Te ha ganado la maquina :(")
+            print("---------------------------------------")
+            print("-                                     -")        
+            print("-       TE GANO LA MAQUINA XD         -")        
+            print("-                                     -")                     
+            print("---------------------------------------")
